67b5dcd291f4438a39ba98f50ad8738c770ce259
Saud Destama <saud.panjaitan@gmail.com>
2025-07-29 12:18:10 +0000

    Fix Google Cloud Storage authentication for Cloud Run

--- src/config/cloudStorage.js
+++ src/config/cloudStorage.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 // Initialize Google Cloud Storage
 const storage = new Storage({
     projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+    // keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
 });
 
 const bucket = storage.bucket(process.env.GOOGLE_CLOUD_BUCKET);
